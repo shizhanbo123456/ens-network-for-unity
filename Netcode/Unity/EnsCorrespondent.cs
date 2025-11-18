57c8c79bf712075834d5576c104eea9826c4cdb3
Zhanbo Shi <shizhanbo123456@163.com>
2025-11-18 15:53:24 +0800

    Add tooltips

--- Netcode/Unity/EnsCorrespondent.cs
+++ Netcode/Unity/EnsCorrespondent.cs
@@ -7,21 +7,28 @@ using UnityEngine;
 
 public class EnsCorrespondent :MonoBehaviour
 {
+    [Tooltip("The ip of the server which you gonna connect")]
     public string IP = "127.0.0.1";
+    [Tooltip("The port of connections, for both server and clients")]
     public int Port = 65432;
     public enum NetworkMode
     {
         None, Server, Client, Host
     }
+    [Tooltip("Which character the current game is at")]
     public NetworkMode networkMode;
 
+    [Tooltip("In which way the client would receive data multithreading or asynchronous")]
     public ProtocolWrapper.ConcurrentType recvMode;
-
+    [Tooltip("Which protocol would be used, make sure it uses the same protocol as the server")]
     public ProtocolWrapper.ProtocolType protocolType;
 
     [Space]
+    [Tooltip("How long the local key will exist, it includes sending and ignoring response")]
     public float KeyExistTime = 5f;//关键信息忽略时长
+    [Tooltip("The interval of unconfirmed keys to send")]
     public float KeySendInterval = 0.2f;//未确认的关键信息发送时长
+    [Tooltip("How long will you ignore key messages after confirmed")]
     public float RKeyExistTime = 5f;//返回的关键信息忽略时长
 
     [Space]
@@ -31,10 +38,12 @@ public class EnsCorrespondent :MonoBehaviour
     /// <summary>
     /// 上次接收心跳检测时间超过此阈值会认为断开了连接
     /// </summary>
+    [Tooltip("How long will a connection be reset since the last message received")]
     public float DisconnectThreshold = 3f;
     /// <summary>
     /// 发送心跳检测消息的间隔
     /// </summary>
+    [Tooltip("The interval to send heartbeat message, it's also used to confirm the delay between client and server")]
     public float HeartbeatMsgInterval = 0.2f;
 
 
